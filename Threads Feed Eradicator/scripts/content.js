d02b306609fd0be6e5313d9bb3782b795d620735
alicephoenixofficial <alice.phoenix.official@gmail.com>
2024-11-03 02:12:08 -0600

    Threads Feed Eradicator
    Threads Feed Eradicator v1.0 initial upload

---
+++ Threads Feed Eradicator/scripts/content.js
@@ -0,0 +1,14 @@
+const url = document.URL.toString();
+
+const path = "x1n2onr6";
+let element13 = document.getElementsByClassName(path);
+
+if (element13) {
+    console.log("Element13: " + element13.length.toString());
+    const child = element13[1];
+    if (child) {
+        const parent = child.parentNode;
+
+        parent.removeChild(child);
+    }
+}
